f382ca2c5e84e90d4f324714a33e5699e0e8bb0b
Shane Slone <slone.shane@gmail.com>
2021-03-11 23:35:34 -0500

    inital commit

---
+++ src/main/java/me/shaneslone/fairshare/FairshareApplication.java
@@ -0,0 +1,13 @@
+package me.shaneslone.fairshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairshareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/shaneslone/fairshare/FairshareApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shaneslone.fairshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairshareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
