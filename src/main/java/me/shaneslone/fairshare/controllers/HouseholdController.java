7ab02041f2f1a98dba24eb8b06176580cde8f494
Shane Slone <slone.shane@gmail.com>
2021-03-16 23:43:52 -0400

    finished household endpoints

--- src/main/java/me/shaneslone/fairshare/controllers/HouseholdController.java
+++ src/main/java/me/shaneslone/fairshare/controllers/HouseholdController.java
@@ -3,12 +3,14 @@ package me.shaneslone.fairshare.controllers;
 import me.shaneslone.fairshare.models.Household;
 import me.shaneslone.fairshare.services.HouseholdService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +24,37 @@ public class HouseholdController {
     public ResponseEntity<?> listAllHouseholds(){
         List<Household> households = householdService.findAll();
         return new ResponseEntity<>(households, HttpStatus.OK);
+    }
+
+    @GetMapping(name = "/household/{householdid}", produces = "application/json")
+    public ResponseEntity<?> getHouseholdById(@PathVariable long householdid){
+        Household household = householdService.findByHouseholdId(householdid);
+        return new ResponseEntity<>(household, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/household", consumes = "application/json", produces = "application/json")
+    public  ResponseEntity<?> addNewHousehold(@Valid @RequestBody Household newHousehold){
+        newHousehold.setHouseholdid(0);
+        newHousehold = householdService.save(newHousehold);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newHouseholdURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{householdid}")
+                .buildAndExpand()
+                .toUri();
+        responseHeaders.setLocation(newHouseholdURI);
+        return new ResponseEntity<>(newHousehold, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/household/{householdid}", consumes = "application/json", produces = "application/sjon")
+    public ResponseEntity<?> updateFullHousehold(@Valid @RequestBody Household updateHousehold, @PathVariable long householdid){
+        updateHousehold.setHouseholdid(householdid);
+        updateHousehold = householdService.save(updateHousehold);
+        return new ResponseEntity<>(updateHousehold, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/household/{householdid}", consumes = "application/json")
+    public ResponseEntity<?> deleteHouseholdById(@PathVariable long householdid){
+        householdService.delete(householdid);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
